84d2e3427768c9668bf5029b509e974bbdc0256a
Steven Oxley <steven.aj.oxley@gmail.com>
2011-02-23 04:53:37 +0000

    Added a title field and moved the model into its own file.

---
+++ home/models/article.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Article(models.Model):
+    created_at = models.DateTimeField("created at", auto_now_add=True)
+    modified_at = models.DateTimeField("modified at", auto_now=True)
+    text = models.TextField()
+    show_on_homepage = models.BooleanField(
+        "show on homepage", default=False)
+    order = models.IntegerField()
+    title = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["order", "-created_at"]
+        app_label = 'home'
