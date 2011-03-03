d2c05ecc76c9d619090e60afed0adb61f94d9016
Steven Oxley <steven.aj.oxley@gmail.com>
2011-03-03 03:32:16 +0000

    Added ulr for index page and created a (very\!) basic view.

--- urls.py
+++ urls.py
@@ -5,11 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^rtoham/', include('rtoham.foo.urls')),
+    (r'^$', 'home.views.index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
