b7035d3ca1a34313c99d3d6488c907d6b9a2b9c9
Steven Oxley <steven.aj.oxley@gmail.com>
2011-11-26 19:31:18 +0000

    Configured models and urls for uploading new pictures.

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,6 @@ from products.models import Picture
 from django.contrib import admin
 
 class PictureAdmin(admin.ModelAdmin):
-    fields = ['alt_text', 'image']
+    fields = ['alt_text', 'original_image']
 
 admin.site.register(Picture, PictureAdmin)
--- products/models/picture.py
+++ products/models/picture.py
@@ -1,14 +1,40 @@
 from django.db import models
 
+class PictureValidators:
+    @staticmethod
+    def validate_aspect_ratio(image_value):
+        """
+        Validates that the aspect ratio of the image is 4:3.
+        """
+        from django.core.exceptions import ValidationError
+        if image_value.width / 4 != image_value.height / 3:
+            raise ValidationError("The image must have a 4:3 aspect ratio.")
+
 class Picture(models.Model):
     created_at = models.DateTimeField("created at", auto_now_add=True)
     modified_at = models.DateTimeField("modified at", auto_now=True)
+    original_image = models.ImageField(upload_to="images/originals",
+                                       height_field="original_height",
+                                       width_field="original_width",
+                                       validators=
+                                       [PictureValidators.validate_aspect_ratio])
     image = models.ImageField(upload_to="images",
                               height_field="height",
-                              width_field="width")
+                              width_field="width",
+                              validators=
+                              [PictureValidators.validate_aspect_ratio])
+    thumbnail = models.ImageField(upload_to="images/thumbnails",
+                                  height_field="thumbnail_height",
+                                  width_field="thumbnail_width",
+                                  validators=
+                                  [PictureValidators.validate_aspect_ratio])
     alt_text = models.TextField()
-    width = models.IntegerField()
-    height = models.IntegerField()
+    original_width = models.IntegerField()
+    original_height = models.IntegerField()
+    width = models.IntegerField(null=True)
+    height = models.IntegerField(null=True)
+    thumbnail_width = models.IntegerField(null=True)
+    thumbnail_height = models.IntegerField(null=True)
 
     def __unicode__(self):
         return self.alt_text
