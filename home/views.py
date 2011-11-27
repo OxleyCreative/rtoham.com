1e98d5e63b3845e43311857b7f63d7a03c9eaa8f
Steven Oxley <steven.aj.oxley@gmail.com>
2011-11-27 20:22:53 +0000

    Updated front page to pull 'used equipment' picture gallery from database.

--- home/views.py
+++ home/views.py
@@ -1,16 +1,23 @@
 from home.models import Article
+from products.models import Product
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     sectionQuery = Article.objects.filter(
         slug__in=['homepage-section-' + str(x) for x in range(1,4)]
     ).order_by('slug')
-    sections = []
-    for article in sectionQuery:
-        sections.append(article)
+    sections = [article for article in sectionQuery]
+
+    equipmentQuery = Product.objects.select_related(
+        "image"
+    ).filter(
+        category="USED_EQUIPMENT"
+    ).order_by("-modified_at")[:5]
+    units = [unit for unit in equipmentQuery]
     return render_to_response("home/index.html",
                               {"currentpage": "home",
-                               "sections": sections})
+                               "sections": sections,
+                               "units": units})
 
 def detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
