d0ad55f3c5cbf362564b4a3cc361379fa8742fec
Steven Oxley <steven.aj.oxley@gmail.com>
2011-12-06 07:51:06 +0000

    Got product list working.

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
     'products.views',
-    (r'$', 'product_list'),
-    (r'(?P<categories>[A-Za-z_,]+)', 'product_list')
+    (r'^$', 'index'),
+    (r'^(?P<category_string>[A-Za-z_,]+)/$', 'product_list')
 )
--- products/views.py
+++ products/views.py
@@ -1,2 +1,34 @@
-from product.models import Product
+import math
+import sys
+from products.models import Product
 from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+    return product_list(request, '')
+
+def product_list(request, category_string):
+    NUM_PER_PAGE = 25
+    PRODUCT_CATEGORIES = [x[0] for x in Product.CATEGORY_CHOICES]
+    
+    category_strings = [x.upper() for x in category_string.split(',')]
+
+    if len(category_strings) == 1 and category_strings[0] == '':
+        category_strings = PRODUCT_CATEGORIES
+
+    # If there is more than one category, then we need to limit
+    # the number of products from each category.
+    if len(category_strings) > 1:
+        limit = math.ceil(NUM_PER_PAGE / len(category_strings))
+    else:
+        limit = sys.maxsize
+        
+    categories = {}
+    for category in category_strings:
+        categories[category] = Product.objects.select_related(
+            'image').filter(category = category)[:limit]
+    
+    return render_to_response(
+        'products/list.html',
+        {'categories': categories,
+         'category_strings': category_strings,
+         'currentpage': 'products'})
