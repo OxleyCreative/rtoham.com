f25ef016780b47e03422aef7c15dbe84c5dd1a01
Steven Oxley <steven.aj.oxley@gmail.com>
2014-11-11 18:20:34 -0800

    Set up uploads folder.
    Fix reverse routing in template.

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import *
 from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,4 +18,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
